4e731913c8f7aa27699cf1cecc152ba8a6caa10a
Sean Nicholas Kropff <117237260+skropff@users.noreply.github.com>
2025-11-08 14:14:27 -0800

    Update dining.c

--- project3-slug-dining-skropff-main/project3-slug-dining-skropff-main/dining.c
+++ project3-slug-dining-skropff-main/project3-slug-dining-skropff-main/dining.c
@@ -55,10 +55,11 @@ void dining_destroy(dining_t **dining) {
 
 void dining_student_enter(dining_t *dining) {
   // TODO: Your code goes here
-  printf("here\n");
   sem_wait(&(dining->semaphore1));
   pthread_mutex_t personal;
+  printf("here\n");
   pthread_mutex_init(&personal, NULL);
+  printf("here1\n");
   int result;
   result = -1;
   /*
